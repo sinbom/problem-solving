66e0fe795cdfc93db824eb913365b576f25b57cc
sinbom <dev.sinbom@gmail.com>
2021-11-18 12:32:13 +0900

    백준(13913, 14226) 문제풀이

---
+++ src/boj/dfs/Boj13913.java
@@ -0,0 +1,81 @@
+package boj.dfs;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Boj13913 {
+
+    public static int[] graph;
+
+    public static int count = 0;
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            String[] s = reader.readLine().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int k = Integer.parseInt(s[1]);
+            graph = new int[100001];
+
+            if (n == k) {
+                writer.write("0\n");
+                writer.write(n + "");
+            } else if (n > k) {
+                writer.write(n - k + "\n");
+                for (int i = n; i >= k; i--) {
+                    writer.write(i + " ");
+                }
+            } else {
+                Arrays.fill(graph, -1);
+                bfs(n, k);
+                writer.write(count + "\n");
+
+                Stack<Integer> stack = new Stack<>();
+
+                for (int i = k; i != n; i = graph[i]) {
+                    stack.push(i);
+                }
+                stack.push(n);
+
+                while (!stack.isEmpty()) {
+                    writer.write(stack.pop() + " ");
+                }
+            }
+        }
+    }
+
+    public static void bfs(int n, int k) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(n);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++) {
+                Integer node = queue.poll();
+
+                if (node == k) {
+                    return;
+                }
+
+                int[] values = new int[]{
+                        node + 1,
+                        node - 1,
+                        node * 2
+                };
+
+                for (int value : values) {
+                    if (value >= 0 && value <= 100000 && graph[value] == -1) {
+                        queue.offer(value);
+                        graph[value] = node;
+                    }
+                }
+            }
+            count++;
+        }
+    }
+
+}
---
+++ src/boj/dfs/Boj14226.java
@@ -0,0 +1,68 @@
+package boj.dfs;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Boj14226 {
+
+    public static boolean[][] check;
+
+    public static class Node {
+
+        private final int n;
+
+        private final int cp;
+
+        private final int t;
+
+        public Node(int n, int cp, int t) {
+            this.n = n;
+            this.cp = cp;
+            this.t = t;
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int s = Integer.parseInt(reader.readLine());
+            check = new boolean[1001][1001];
+
+            bfs(s);
+        }
+    }
+
+    public static void bfs(int s) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(1, 0, 0));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+
+            if (node.n == s) {
+                System.out.println(node.t + "");
+                return;
+            }
+
+            if (node.n > 0 && node.n <= 1000) {
+                if (!check[node.n][node.n]) { // 복사
+                    queue.offer(new Node(node.n, node.n, node.t + 1));
+                    check[node.n][node.n] = true;
+                }
+                if (!check[node.n - 1][node.cp]) { // 제거
+                    queue.offer(new Node(node.n - 1, node.cp, node.t + 1));
+                    check[node.n - 1][node.cp] = true;
+                }
+            }
+
+            if (node.cp > 0 && node.n + node.cp <= 1000 && !check[node.n + node.cp][node.cp]) {
+                queue.offer(new Node(node.n + node.cp, node.cp, node.t + 1));
+                check[node.n + node.cp][node.cp] = true;
+            }
+        }
+
+    }
+
+}
